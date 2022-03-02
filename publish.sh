4f5b546be54f1075aa6fac6e67f3d2c1ef0f4d7f
Tom <trobinson@linius.com>
2022-03-02 19:27:35 +0000

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DOMAIN_OWNER="774774188620"
+DOMAIN="linius"
+REGION="ap-southeast-2"
+REPOSITORY="pypi-store"
+
+aws codeartifact login \
+    --tool twine \
+    --domain $DOMAIN \
+    --domain-owner $DOMAIN_OWNER \
+    --repository $REPOSITORY \
+    --region $REGION
+
+rm -rf dist/
+python -m pip install --upgrade build
+python -m build
+
+twine upload -r codeartifact dist/*
